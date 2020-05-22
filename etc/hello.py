dcef572bf64bfc827b8e83036851fcb039de8ea4
mikle97pir <mikle97pir@yandex.ru>
2020-05-23 01:10:30 +0300

    added gunicorn

---
+++ etc/hello.py
@@ -0,0 +1,9 @@
+def app(environ, start_response):
+        s = environ["QUERY_STRING"]
+        data = "\n".join(s.split("&"))
+        data = (data + "\n").encode()
+        start_response("200 OK", [
+            ("Content-Type", "text/plain"),
+            ("Content-Length", str(len(data)))
+        ])
+        return iter([data])
--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
-  sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-  sudo rm -rf /etc/nginx/sites-enabled/default
-  sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
+killall -q gunicorn
+gunicorn -b 0.0.0.0:8080 -w 4 --chdir etc -D --error-logfile=/home/box/gunicorn.log --capture-output hello:app
+sudo /etc/init.d/nginx restart
