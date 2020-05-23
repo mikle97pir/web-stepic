e6daf325a625fd907a762ef3c7323fceb383a5d1
mikle97pir <mikle97pir@yandex.ru>
2020-05-24 01:09:56 +0300

    switched to gunicorn3

--- hello.py
+++
@@ -1,9 +0,0 @@
-def app(environ, start_response):
-        s = environ["QUERY_STRING"]
-        data = "\n".join(s.split("&"))
-        data = (data + "\n").encode()
-        start_response("200 OK", [
-            ("Content-Type", "text/plain"),
-            ("Content-Length", str(len(data)))
-        ])
-        return iter([data])
--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 killall -q gunicorn
-gunicorn -b 0.0.0.0:8080 -w 4 --chdir etc -D --error-logfile=/home/box/gunicorn.log --capture-output hello:app
+gunicorn3 -b 0.0.0.0:8080 -w 4 --chdir /home/box/web/etc -D --error-logfile=/home/box/gunicorn.log --capture-output hello:app
 sudo /etc/init.d/nginx restart
