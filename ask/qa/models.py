acc24de4d018d55e8ceaddbf58fbd1569c6a149d
mikle97pir <mikle97pir@yandex.ru>
2020-06-15 04:04:42 +0300

    meow

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -13,10 +13,10 @@ class QuestionManager(models.Manager):
 
 class Question(models.Model):
     objects = QuestionManager()
-    title = models.CharField(max_length=255)
-    text = models.TextField()
-    added_at = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField(default=0)
+    title = models.CharField(max_length=255, null=True, default='')
+    text = models.TextField(null=True, default='')
+    added_at = models.DateTimeField(auto_now_add=True, null=True)
+    rating = models.IntegerField(default=0, null=True)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     likes = models.ManyToManyField(User, related_name='question_like_user')
     def __str__(self):
@@ -25,8 +25,8 @@ class Question(models.Model):
         return reverse('question-id', kwargs={'id': self.id})
 
 class Answer(models.Model):
-    text = models.TextField()
-    added_at = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(null=True, default='')
+    added_at = models.DateTimeField(auto_now_add=True, null=True)
     question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     def __str__(self):
