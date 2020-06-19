b795d5f3adb7e08badafa744d9539e86796c6b61
mikle97pir <mikle97pir@yandex.ru>
2020-06-20 01:29:26 +0300

    minor

--- create_database.sql
+++ create_database.sql
@@ -1,4 +1,4 @@
-create database ask;
+create database ask character set UTF8 collate utf8_bin;
 use ask;
 create user 'django_app'@'localhost' identified by 'django_pass';
 grant all on ask.* to 'django_app'@'localhost' identified by 'django_pass';
